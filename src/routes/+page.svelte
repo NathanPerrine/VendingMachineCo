63be3f1d7c9e7544cb1fc22ceee888844f617fd3
Nathan Perrine <nathan.perrine@gmail.com>
2024-03-28 13:29:36 -0400

    tailwind setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,15 @@
-test 123
+<script lang="ts">
+
+
+</script>
+
+
+<h1 class="text-7xl font-bold underline">
+  Hello world!
+</h1>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
