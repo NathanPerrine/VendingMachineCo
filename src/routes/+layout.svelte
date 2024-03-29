776a367ea0808543590abb5dae523267d333313f
Nathan Perrine <nathan.perrine@gmail.com>
2024-03-29 16:08:41 -0400

    messing with formatting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
     import type { LayoutData } from './$types';
     import "../app.css"
+    import Header from '$lib/components/header/header.svelte';
     export let data: LayoutData;
 </script>
 
+<Header />
 <slot />
