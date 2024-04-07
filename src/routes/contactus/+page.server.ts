bc6ff6beda61c07a02ba4e9ff896ab62fdad8a44
Nathan Perrine <nathan.perrine@gmail.com>
2024-04-07 01:20:25 -0400

    email sending complete

--- src/routes/contactus/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import type { PageServerLoad } from './$types';
-
-export const actions = {
-  sendEmail: async ({ cookies, request }) => {
-    console.log('hello world')
-    const data = await request.formData();
-    console.log(data)
-  },
-};
